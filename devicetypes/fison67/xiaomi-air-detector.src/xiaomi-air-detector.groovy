3b9ded488767e949bc2742b824932e2aa7a3279a
shin4299 <shin4299@gmail.com>
2019-01-13 21:40:08 +0900

    Update xiaomi-air-detector.groovy

--- devicetypes/fison67/xiaomi-air-detector.src/xiaomi-air-detector.groovy
+++ devicetypes/fison67/xiaomi-air-detector.src/xiaomi-air-detector.groovy
@@ -32,52 +32,21 @@ import groovy.transform.Field
 
 @Field 
 LANGUAGE_MAP = [
-    "display": [
-        "Korean": "화면\n모드",
-        "English": "Display\nMode"
-    ],
-    "night": [
-        "Korean": "취침\n모드",
-        "English": "Night\nMode"
-    ],
-    "power": [
-        "Korean": "전원",
-        "English": "Power\nSource"
-    ],
-    "refresh": [
-        "Korean": "새로\n고침",
-        "English": "Refresh"
-    ],
-    "begin": [
-        "Korean": "취침 모드\n시작 시간",
-        "English": "Night Mode\nBegin Time"
-    ],
-    "end": [
-        "Korean": "취침 모드\n종료 시간",
-        "English": "Night Mode\nEnd Time"
-    ],
-    "setup": [
-        "Korean": " 시간 \n 설정후 \n 누름",
-        "English": " Tap \n After \n Time \n Set "
-    ]
 ]
 
 metadata {
 	definition (name: "Xiaomi Air Detector", namespace: "fison67", author: "fison67", mnmn: "SmartThings", vid: "generic-switch") {
         capability "Air Quality Sensor"						//"on", "off"
-		capability "Relative Humidity Measurement"
-		capability "Temperature Measurement"
+	capability "Relative Humidity Measurement"
+	capability "Temperature Measurement"
+	capability "Tvoc Measurement"
         capability "Carbon Dioxide Measurement"
         capability "Refresh"
-		capability "Refresh"
-		capability "Sensor"
-		capability "Power Source"
-		capability "Dust Sensor" // fineDustLevel : PM 2.5   dustLevel : PM 10
+	capability "Refresh"
+	capability "Sensor"
+	capability "Power Source"
+	capability "Dust Sensor" // fineDustLevel : PM 2.5   dustLevel : PM 10
 
-        attribute "clock", "enum", ["on", "off"]
-        attribute "night", "enum", ["on", "off"]
-        attribute "setbeap", "enum", ["am", "pm"]
-        attribute "setendap", "enum", ["am", "pm"]
         
         attribute "lastCheckin", "Date"
      
@@ -104,11 +73,10 @@ metadata {
 	simulator {
 	}
 	preferences {
-	        input name: "selectedLang", title:"Select a language" , type: "enum", required: true, options: ["English", "Korean"], defaultValue: "English", description:"Language for DTH"
 	}
 
 	tiles {
-		multiAttributeTile(name:"fineDustLevel", type: "generic", width: 6, height: 4){
+		multiAttributeTile(name:"fineDustLevel", type: "generic", width: 3, height: 2){
 			tileAttribute ("device.fineDustLevel", key: "PRIMARY_CONTROL") {
                 attributeState "default", label:'${currentValue}㎍/㎥', unit:"㎍/㎥", backgroundColors:[
 			[value: -1, color: "#C4BBB5"],
@@ -126,26 +94,27 @@ metadata {
             tileAttribute("device.lastCheckin", key: "SECONDARY_CONTROL") {
     			attributeState("default", label:'\nLast Update: ${currentValue}')
             }
-		}
-		valueTile("pm25", "device.fineDustLevel", decoration: "flat", width: 2, height: 2) {
-        	state "default", label:'${currentValue}㎍/㎥', icon:"http://postfiles9.naver.net/MjAxODA0MDNfMjkw/MDAxNTIyNzI3NjY0Mzk0.yVQdGxRJMGFrGQLVzb-OUThZptHXIBmTaMEZO3LoipAg.v0Rw0_zvHr7wBk-VeH5KQxNry_zUOz4aXUn6I1QQ9xkg.PNG.shin4299/pm25_on.png?type=w3", unit:"㎍/㎥", backgroundColors:[
-			[value: -1, color: "#C4BBB5"],
-            		[value: 0, color: "#7EC6EE"],
-            		[value: 15, color: "#51B2E8"],
-            		[value: 50, color: "#e5c757"],
-            		[value: 75, color: "#E40000"],
-            		[value: 500, color: "#970203"]
-            ]
-        }
+	}
+		valueTile("tvoc", "device.tvocLevel", decoration: "flat", width: 2, height: 2) {
+        		state "default", label:'${currentValue}㎍/㎥', unit:"㎍/㎥", backgroundColors:[
+				[value: 0.3, color: "#18cdff"],
+            			[value: 1, color: "#19ffeb"],
+            			[value: 3, color: "#ddf927"],
+            			[value: 9, color: "#ffb71e"],
+            			[value: 100, color: "#f94d1d"]
+            		]
+        	}
  		valueTile("carbonDioxide", "device.carbonDioxide", width: 2, height: 2, inactiveLabel: false) {
  			state "carbonDioxide", label:'${currentValue}ppm', unit:"CO2", backgroundColors: [
- 				[value: 600, color: "#44B621"],
-                [value: 999, color: "#ffcc00"],
-                [value: 1000, color: "#e86d13"]
+ 				[value: 600, color: "#18cdff"],
+                		[value: 999, color: "#19ffeb"],
+                		[value: 1500, color: "#ddf927"],
+                		[value: 2000, color: "#ffb71e"],
+                		[value: 6000, color: "#f94d1d"],
  				]
  		}
-		valueTile("temperature", "device.temperature") {
- 			state("temperature", label: '${currentValue}°', icon:"st.Weather.weather2", backgroundColors: [
+		valueTile("temperature", "device.temperature", width: 2, height: 2, inactiveLabel: false) {
+ 			state("temperature", label: '${currentValue}°', backgroundColors: [
  				[value: 31, color: "#153591"],
  				[value: 44, color: "#1e9cbb"],
  				[value: 59, color: "#90d2a7"],
@@ -156,18 +125,28 @@ metadata {
  				]
  				)
  		}        
-        valueTile("display_label", "device.display_label", decoration: "flat") {
-            state "default", label:'${currentValue}'
-        }        
-        valueTile("night_label", "device.night_label", decoration: "flat") {
-            state "default", label:'${currentValue}'
-        }
-        valueTile("power_label", "device.power_label", decoration: "flat") {
-            state "default", label:'${currentValue}'
-        }
-        valueTile("refresh_label", "device.refresh_label", decoration: "flat") {
-            state "default", label:'${currentValue}'
-        }
+		valueTile("humidity", "device.humidity", width: 2, height: 2, inactiveLabel: false) {
+ 			state("humidity", label: '${currentValue}%', backgroundColors: [
+ 				[value: 20, color: "#f94d1d"],
+ 				[value: 40, color: "#ffb71e"],
+ 				[value: 60, color: "#ddf927"],
+ 				[value: 80, color: "#19ffeb"],
+ 				[value: 100, color: "#18cdff"]
+ 				]
+ 				)
+ 		}        
+        	valueTile("display_label", "device.display_label", decoration: "flat") {
+            		state "default", label:'${currentValue}'
+        	}        
+        	valueTile("night_label", "device.night_label", decoration: "flat") {
+            		state "default", label:'${currentValue}'
+        	}
+        	valueTile("power_label", "device.power_label", decoration: "flat") {
+            		state "default", label:'${currentValue}'
+        	}
+        	valueTile("refresh_label", "device.refresh_label", decoration: "flat") {
+            		state "default", label:'${currentValue}'
+        	}
         valueTile("timeset_label", "device.timeset_label", decoration: "flat", width: 1, height: 2) {
             state "default", label:'${currentValue}', action:"setUpTime"
         }
@@ -176,47 +155,10 @@ metadata {
             state("val", label:'${currentValue}%', defaultState: true, backgroundColor:"#00a0dc")
         }
         
-        standardTile("refresh", "device.refresh") {
-            state "default", label:"", action:"refresh", icon:"st.secondary.refresh", backgroundColor:"#A7ADBA"
-        }
-        valueTile("setbe_label", "device.setbe_label", decoration: "flat", width: 2, height: 1) {
-            state "default", label:'${currentValue}'
-        }
-        valueTile("setend_label", "device.setend_label", decoration: "flat", width: 2, height: 1) {
-            state "default", label:'${currentValue}'
-        }
-        standardTile("setendap", "device.setendap", decoration: "flat") {
-            state "am", label:"am", action:"setEndPm", icon:"st.Weather.weather14", backgroundColor:"#edd884", nextState:"am.."
-            state "pm", label:"pm", action:"setEndAm", icon:"st.Weather.weather4", backgroundColor:"#7a88bc", nextState:"pm.."
 
-            state "am..", label:"..", action:"setEndPm", icon:"st.Weather.weather14", backgroundColor:"#edd884", nextState:"pm.."
-            state "pm..", label:"..", action:"setEndPm", icon:"st.Weather.weather4", backgroundColor:"#7a88bc", nextState:"am.."
-        }
         
-        valueTile("setbe_hour", "device.setbe_hour", inactiveLabel: false, decoration: "flat") {
-            state "default", label:'${currentValue}h', action:"setBeHour"
-            }
-        valueTile("setend_hour", "device.setend_hour", inactiveLabel: false, decoration: "flat") {
-            state "default", label:'${currentValue}h', action:"setEndHour"
-        }
-        valueTile("setbe_min", "device.setbe_min", inactiveLabel: false, decoration: "flat") {
-            state "default", label:'${currentValue}min', action:"setBeMin"
-        }
-        valueTile("setend_min", "device.setend_min", inactiveLabel: false, decoration: "flat") {
-            state "default", label:'${currentValue}min', action:"setEndMin"
-        }
-        standardTile("setup_be", "device.setup_be", inactiveLabel: false, decoration: "flat") {
-            state "default", label:"Set\nUp", action:"setupBe", icon:"st.secondary.refresh"
-        }
-        standardTile("setup_end", "device.setup_end", inactiveLabel: false, decoration: "flat") {
-            state "default", label:"Set\nUp", action:"setupEnd", icon:"st.secondary.refresh"
-        }
-        
-        main (["pm25"])
-		details(["fineDustLevel", "switch", "display_label", "night_label", "power_label", "refresh_label", 
-    		"clock", "night", "powerSource", "refresh",
-            "setbe_label", "setbe_hour", "setbe_min", "setbeap", "timeset_label", 
-            "setend_label", "setend_hour", "setend_min", "setendap"])
+        main (["fineDustLevel"])
+		details(["fineDustLevel", "tvoc", "carbonDioxide", "temperature", "humidity"])
 		
 	}
 }
@@ -239,146 +181,24 @@ def setStatus(params){
     case "pm2.5":
     	sendEvent(name:"fineDustLevel", value: params.data)
     	break;
-    case "aqi":
-    	sendEvent(name:"fineDustLevel", value: params.data)
+    case "temperature":
+    	sendEvent(name:"temperature", value: params.data)
     	break;
-    case "power":
-    	sendEvent(name:"switch", value: (params.data == "true" ? "on" : "off"))
+    case "tvoc":
+    	sendEvent(name:"tvocLevel", value: params.data)
+    	break;
+    case "relativeHumidity":
+    	sendEvent(name:"humidity", value: params.data)
+    	break;
+    case "co2e":
+    	sendEvent(name:"carbonDioxide", value: params.data)
     	break;
     case "battery":
     	sendEvent(name:"battery", value: params.data)
         break;
-    case "usb_state":
-    	sendEvent(name:"powerSource", value: (params.data == "on" ? "dc" : "battery"))
-    	break;
-    case "nightState":
-    	sendEvent(name:"night", value: (params.data == "on" ? "on" : "off"))
-    	break;
-    case "timeState":
-    	sendEvent(name:"clock", value: (params.data == "on" ? "on" : "off"))
-    	break;
-    case "nightBeginTime":
-    	break;
-    case "nightEndTime":
-    	break;
-    case "sensorState":
-    	sendEvent(name:"sensor", value: (params.data == "on" ? "on" : "off"))
-    	break;
     }
     
     updateLastTime()
-}
-def setBeHour() {
-	if(device.currentValue('setbe_hour') == null){
-    sendEvent(name: "setbe_hour", value: 1 )
-    } else {
-	def h = device.currentValue('setbe_hour')
-	int be = Float.parseFloat(h)    
-	state.setBeHour = be
-	if(state.setBeHour >= 12) {
-    state.setBeHour = 1
-    } else {
-	state.setBeHour = state.setBeHour + 1
-    }
-	log.debug "setbe_hour '${state.setBeHour}'"    
-    sendEvent(name: "setbe_hour", value: state.setBeHour)
-    }
-}    
-    
-def setEndHour() {
-	if(device.currentValue('setend_hour') == null){
-    sendEvent(name: "setend_hour", value: 1 )
-    } else {
-	def h = device.currentValue('setend_hour')
-	int be = Float.parseFloat(h)    
-	state.setEndHour = be
-	if(state.setEndHour >= 12) {
-    state.setEndHour = 1
-    } else {
-	state.setEndHour = state.setEndHour + 1
-    }
-	log.debug "setend_hour '${state.setEndHour}'"    
-    sendEvent(name: "setend_hour", value: state.setEndHour)
-    }
-}    
-        
-def setBeMin() {
-	if(device.currentValue('setbe_min') == null){
-    sendEvent(name: "setbe_min", value: 10 )
-    } else {
-	def h = device.currentValue('setbe_min')
-	int be = Float.parseFloat(h)    
-	state.setBeMin = be
-	if(state.setBeMin >= 50) {
-    state.setBeMin = 0
-    } else {
-	state.setBeMin = state.setBeMin + 10
-    }
-	log.debug "setBeMin '${state.setBeMin}'"    
-    sendEvent(name: "setbe_min", value: state.setBeMin)
-    }
-}
-
-def setEndMin() {
-	if(device.currentValue('setend_min') == null){
-    sendEvent(name: "setend_min", value: 10 )
-    } else {
-	def h = device.currentValue('setend_min')
-	int be = Float.parseFloat(h)    
-	state.setEndMin = be
-	if(state.setEndMin >= 50) {
-    state.setEndMin = 0
-    } else {
-	state.setEndMin = state.setEndMin + 10
-    }
-	log.debug "setEndMin '${state.setEndMin}'"    
-    sendEvent(name: "setend_min", value: state.setEndMin)
-    }
-}
-
-def setUpTime(){
-	log.debug "setUpTime >> ${state.id}"
-	if(device.currentValue('setbeap') == 'am'){
-    		if(device.currentValue('setbe_hour') == '12'){
-        	state.beHour = 23
-            } else { state.beHour = Integer.parseInt(device.currentValue('setbe_hour')) - 1
-            }
-     } else {
-     		if(device.currentValue('setbe_hour') == '12'){
-        	state.beHour = 11
-            } else { state.beHour = Integer.parseInt(device.currentValue('setbe_hour')) + 11
-            }
-     }
-    if(device.currentValue('setendap') == 'am'){
-    		if(device.currentValue('setend_hour') == '12'){
-        	state.endHour = 23
-            } else { state.endHour = Integer.parseInt(device.currentValue('setend_hour')) - 1
-            }
-     } else {
-     		if(device.currentValue('setend_hour') == '12'){
-        	state.endHour = 11
-            } else { state.endHour = Integer.parseInt(device.currentValue('setend_hour')) + 11
-            }
-     }
-    def beHour = state.beHour
-    def endHour = state.endHour
-    def beMin = Integer.parseInt(device.currentValue('setbe_min'))
-    def endMin = Integer.parseInt(device.currentValue('setend_min'))
-    
-	log.debug "setUpTime >> Begin ${beHour}h ${beMin}min End ${endHour}h ${endMin}min"
-    log.debug "setUpTime >> Begin ${device.currentValue('setbeap')}h ${device.currentValue('setbe_hour')}min End ${endHour}h ${endMin}min"
-    def body = [
-        "id": state.id,
-        "cmd": "nightTime",
-        "data": [ 
-				"beginHour": beHour, 
-				"beginMinute": beMin, 
-				"endHour": endHour, 
-				"endMinute": endMin
-				]
-    ]
-    def options = makeCommand(body)
-    sendCommand(options, null)
 }
 
 def updateLastTime(){
@@ -399,87 +219,6 @@ def refresh(){
     sendCommand(options, callback)
 }
 
-def on(){
-	log.debug "On >> ${state.id}"
-    def body = [
-        "id": state.id,
-        "cmd": "power",
-        "data": "on"
-    ]
-    def options = makeCommand(body)
-    sendCommand(options, null)
-}
-
-def off(){
-	log.debug "Off >> ${state.id}"
-	def body = [
-        "id": state.id,
-        "cmd": "power",
-        "data": "off"
-    ]
-    def options = makeCommand(body)
-    sendCommand(options, null)
-}
-def clockOn(){
-	log.debug "Clock Mode On >> ${state.id}"
-    def body = [
-        "id": state.id,
-        "cmd": "timeState",
-        "data": "on"
-    ]
-    def options = makeCommand(body)
-    sendCommand(options, null)
-}
-def clockOff(){
-	log.debug "Clock Mode Off >> ${state.id}"
-    def body = [
-        "id": state.id,
-        "cmd": "timeState",
-        "data": "off"
-    ]
-    def options = makeCommand(body)
-    sendCommand(options, null)
-}
-def nightOn(){
-	log.debug "Night Mode On >> ${state.id}"
-    def body = [
-        "id": state.id,
-        "cmd": "nightState",
-        "data": "on"
-    ]
-    def options = makeCommand(body)
-    sendCommand(options, null)
-}
-def nightOff(){
-	log.debug "Night Mode Off >> ${state.id}"
-    def body = [
-        "id": state.id,
-        "cmd": "nightState",
-        "data": "off"
-    ]
-    def options = makeCommand(body)
-    sendCommand(options, null)
-}
-
-def setBeAm() {
-	sendEvent(name:"setbeap", value: "am")
-    log.debug "setBeginAP >> am"
-}
-
-def setBePm() {
-	sendEvent(name:"setbeap", value: "pm" )
-    log.debug "setBeginAP >> pm"
-}
-
-def setEndAm() {
-	sendEvent(name:"setendap", value: "am")
-    log.debug "setEndAP >> am"
-}
-
-def setEndPm() {
-	sendEvent(name:"setendap", value: "pm")
-    log.debug "setEndAP >> pm"
-}
 
 def callback(physicalgraph.device.HubResponse hubResponse){
 	def msg
@@ -504,80 +243,11 @@ def callback(physicalgraph.device.HubResponse hubResponse){
     }
 }
 
-def beginTime() {
-    def minutes = ((state.BeginTime/60).intValue() % 60).intValue()
-    def hours = ((state.BeginTime/(60*60)).intValue() %24).intValue()
-    log.debug "refresh begin time '${hours}'/'${minutes}'"    
-	if(hours < 11) {
-		sendEvent(name:"setbeap", value: "am" )
-		sendEvent(name:"setbe_hour", value: hours + 1 )
-		sendEvent(name:"setbe_min", value: minutes )
-    } else if(hours == 11) {
-		sendEvent(name:"setbeap", value: "pm" )
-		sendEvent(name:"setbe_hour", value: hours + 1 )
-		sendEvent(name:"setbe_min", value: minutes )
-    } else if(hours == 23) {
-		sendEvent(name:"setbeap", value: "am" )
-		sendEvent(name:"setbe_hour", value: hours - 11 )
-		sendEvent(name:"setbe_min", value: minutes )
-    } else if(hours == 24) {
-		sendEvent(name:"setbeap", value: "am" )
-		sendEvent(name:"setbe_hour", value: 1 )
-		sendEvent(name:"setbe_min", value: minutes )
-    } else {
-		sendEvent(name:"setbeap", value: "pm" )
-		sendEvent(name:"setbe_hour", value: hours - 11 )
-		sendEvent(name:"setbe_min", value: minutes )
-    }
-}
-    
-def endTime() {
-    def minutes = ((state.EndTime/60).intValue() % 60).intValue()
-    def hours = ((state.EndTime/(60*60)).intValue() %24).intValue()
-    log.debug "refresh end time '${hours}'/'${minutes}'"    
-
-	if(hours < 11) {
-		sendEvent(name:"setendap", value: "am" )
-		sendEvent(name:"setend_hour", value: hours + 1 )
-		sendEvent(name:"setend_min", value: minutes )
-    } else if(hours == 11) {
-		sendEvent(name:"setendap", value: "pm" )
-		sendEvent(name:"setend_hour", value: hours + 1 )
-		sendEvent(name:"setend_min", value: minutes )
-    } else if(hours == 23) {
-		sendEvent(name:"setendap", value: "am" )
-		sendEvent(name:"setend_hour", value: hours - 11 )
-		sendEvent(name:"setend_min", value: minutes )
-    } else if(hours == 24) {
-		sendEvent(name:"setendap", value: "am" )
-		sendEvent(name:"setend_hour", value: 1 )
-		sendEvent(name:"setend_min", value: minutes )
-    } else {
-		sendEvent(name:"setendap", value: "pm" )
-		sendEvent(name:"setend_hour", value: hours - 11 )
-		sendEvent(name:"setend_min", value: minutes )
-    }
-}
-        
-
-
 def updated() {
     refresh()
     setLanguage(settings.selectedLang)
 }
 
-def setLanguage(language){
-    log.debug "Languge >> ${language}"
-	state.language = language
-	
-	sendEvent(name:"display_label", value: LANGUAGE_MAP["display"][language] )
-	sendEvent(name:"night_label", value: LANGUAGE_MAP["night"][language] )
-	sendEvent(name:"power_label", value: LANGUAGE_MAP["power"][language] )
-	sendEvent(name:"refresh_label", value: LANGUAGE_MAP["refresh"][language] )
-	sendEvent(name:"setbe_label", value: LANGUAGE_MAP["begin"][language] )
-	sendEvent(name:"setend_label", value: LANGUAGE_MAP["end"][language] )
-	sendEvent(name:"timeset_label", value: LANGUAGE_MAP["setup"][language] )
-}
 def sendCommand(options, _callback){
 	def myhubAction = new physicalgraph.device.HubAction(options, null, [callback: _callback])
     sendHubCommand(myhubAction)
